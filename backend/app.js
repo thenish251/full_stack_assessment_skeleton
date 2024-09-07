08d5ba5187bd7fe4ed0593802c5c7f5dd21221ad
thenish51 <mddanish107@yahoo.com>
2024-09-07 19:51:47 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/db');
 const propertyRoutes = require('./routes/propertyRoutes');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, 
+  };
+  
+  app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use('/api', propertyRoutes);
 
